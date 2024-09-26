8d3463c1b1145fa92eeb0c73d0d4c2ad9db35b96
oweslley03 <wesleymoraeswv03@hotmail.com>
2024-09-25 21:43:28 -0300

    config api.ts for server init and add delete-user-express-route

---
+++ src/infra/api/express/routes/user/delete-user-express-route.ts
@@ -0,0 +1,39 @@
+import { Request, Response } from "express";
+import { DeleteUserInputDto, DeleteUserUsecase } from "../../../../../usecases/user/delete-user-usecase";
+import { HttpMethod, Route } from "../routes";
+
+export type DeleteUserResponseDto = {
+  success: boolean;
+};
+
+export class DeleteUserRoute implements Route {
+  private constructor(
+    private readonly path: string,
+    private readonly method: HttpMethod,
+    private readonly deleteUserService: DeleteUserUsecase
+  ) {};
+
+  public static create(deleteUserService: DeleteUserUsecase) {
+    return new DeleteUserRoute(
+      '/user-delete',
+      HttpMethod.DELETE,
+      deleteUserService,
+    );
+  };
+
+  public getHandler() {
+    return async (req: Request, res: Response) => {
+      const { User_Id } = req.body;
+      const input: DeleteUserInputDto = { User_Id }; 
+      const output: DeleteUserResponseDto = await this.deleteUserService.execute(input);
+      res.status(202).json(output).send();
+    };
+  };
+
+  public getPath(): string {
+    return this.path;
+  };
+  public getMethod(): HttpMethod {
+    return this.method;
+  };
+};
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,33 @@
-console.log(`app running...`)
+import { FindUserByIdUserRoute } from "./infra/api/express/routes/user/finduserbyid-user-express-route";
+import { RegisterUserRoute } from "./infra/api/express/routes/user/register-user-express-route";
+import { UserRepositoryPrisma } from "./infra/repositories/user/prisma/user-prisma-repository";
+import { UpdateUserRoute } from "./infra/api/express/routes/user/update-user-express-route";
+import { DeleteUserRoute } from "./infra/api/express/routes/user/delete-user-express-route";
+import { FindUserByIdUsecase } from "./usecases/user/finduserbyid-user-usecase";
+import { RegisterUserUsecase } from "./usecases/user/register-user-usecase";
+import { DeleteUserUsecase } from "./usecases/user/delete-user-usecase";
+import { UpdateUserUsecase } from "./usecases/user/update-user-usecase";
+import { ApiExpress } from "./infra/api/express/api.express";
+import { prisma } from "./src/package/prisma/prisma";
+
+
+function main() {
+  const aRepository = UserRepositoryPrisma.create(prisma);
+  
+  const registerUserUsecase = RegisterUserUsecase.create(aRepository);
+  const updateUserUsecase = UpdateUserUsecase.create(aRepository);
+  const findByIdUserUsecase = FindUserByIdUsecase.create(aRepository);
+  const deleteUserUsecase = DeleteUserUsecase.create(aRepository);
+
+  const registerUserRoute = RegisterUserRoute.create(registerUserUsecase);
+  const updateUserRoute = UpdateUserRoute.create(updateUserUsecase);
+  const findByIdUserRoute = FindUserByIdUserRoute.create(findByIdUserUsecase);
+  const deleteUserRoute = DeleteUserRoute.create(deleteUserUsecase);
+
+  const api = ApiExpress.create([registerUserRoute, updateUserRoute, findByIdUserRoute, deleteUserRoute]);
+
+  const port = 8000;
+  api.start(port);
+};
+
+main();
