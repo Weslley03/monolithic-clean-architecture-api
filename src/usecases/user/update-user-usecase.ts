474184491da254a6a8b5abebfcea0d21d98535e7
oweslley03 <wesleymoraeswv03@hotmail.com>
2024-10-28 09:43:28 -0300

    deleting commented code

--- src/usecases/user/update-user-usecase.ts
+++ src/usecases/user/update-user-usecase.ts
@@ -23,17 +23,6 @@ export class UpdateUserUsecase implements Usecase<UpdateUserInputDto, UpdateUser
     const userDto = await this.userGateway.findUserById(input.User_Id);
     if(!userDto) throw new Error('user not found');
 
-    /*const user = User.create(
-      userDto.User_Name,
-      userDto.User_Email,
-      userDto.User_Username,
-      userDto.User_Avatar,
-    ); 
-
-    if(input.newAvatar) user.updateAvatar(input.newAvatar);
-    if(input.newName) user.updateName(input.newName);
-    if(input.newUsername) user.updateUsername(input.newUsername); */
-
     if(input.newAvatar) userDto.User_Avatar = input.newAvatar;
     if(input.newName) userDto.User_Name = input.newName;
     if(input.newUsername) userDto.User_Username = input.newUsername;  
