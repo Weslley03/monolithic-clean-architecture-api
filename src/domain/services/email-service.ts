818bf18f7e26dc9449148ca3091a661ba8c7ef36
oweslley03 <wesleymoraeswv03@hotmail.com>
2024-11-06 23:18:37 -0300

    required call `CLIENT_POSTMARK_KEY` via .env

--- src/domain/services/email-service.ts
+++ src/domain/services/email-service.ts
@@ -1,6 +1,9 @@
 import { ServerClient } from "postmark";
 
-var client = new ServerClient("3e53ea17-b99b-4666-8482-0e88dac35a45");
+const CLIENT_POSTMARK_KEY = process.env.CLIENT_POSTMARK_KEY
+if(!CLIENT_POSTMARK_KEY) throw new Error('could not acccess CLIENT_POSTMARK_KEY variable');
+
+var client = new ServerClient(CLIENT_POSTMARK_KEY);
 
 export async function sendPasswordResetEmail(to: string, token: string) {
   await client.sendEmail({
